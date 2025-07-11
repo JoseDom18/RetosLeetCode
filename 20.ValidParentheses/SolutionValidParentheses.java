91561029568ed188156a9eb08f5df5caf7b212f8
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-07-10 22:43:23 -0600

    solucion 20 aprendida

--- 20.ValidParentheses/SolutionValidParentheses.java
+++ 20.ValidParentheses/SolutionValidParentheses.java
@@ -1,31 +1,51 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class SolutionValidParentheses {
   public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    Map<Character, Character> mapping = new HashMap<>();
+    mapping.put(')', '(');
+    mapping.put('}', '{');
+    mapping.put(']', '[');
 
-    boolean wltl = true;
-    char[] palabra = s.toCharArray();
+    for (char c : s.toCharArray()) {
+      if (mapping.containsValue(c)) {
+        stack.push(c);
+      } else if (mapping.containsKey(c)) {
+        if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+          return false;
+        }
 
-    Map<Character, Integer> map = new HashMap<>();
-
-    for (char a : palabra) {
-      map.put(a, map.getOrDefault(a, 0) + 1);
+      }
     }
-
-    if ((map.getOrDefault('(', 0) - map.getOrDefault(')', 0) == 0)
-        && (map.getOrDefault('[', 0) - map.getOrDefault(']', 0) == 0)
-        && (map.getOrDefault('{', 0) - map.getOrDefault('}', 0) == 0)) {
-      System.out.println("Es momento de analizar");
-    } else {
-      wltl = false;
-    }
-
-
-
-    
-    
-    return wltl;
-
+    return stack.isEmpty();
   }
 }
+
+/*
+ * Como lo queria hacer
+ * boolean wltl = true;
+ * char[] palabra = s.toCharArray();
+ * 
+ * Map<Character, Integer> map = new HashMap<>();
+ * 
+ * for (char a : palabra) {
+ * map.put(a, map.getOrDefault(a, 0) + 1);
+ * }
+ * 
+ * if ((map.getOrDefault('(', 0) - map.getOrDefault(')', 0) == 0)
+ * && (map.getOrDefault('[', 0) - map.getOrDefault(']', 0) == 0)
+ * && (map.getOrDefault('{', 0) - map.getOrDefault('}', 0) == 0)) {
+ * System.out.println("Es momento de analizar");
+ * } else {
+ * wltl = false;
+ * }
+ * 
+ * 
+ * 
+ * 
+ * 
+ * return wltl;
+ */
