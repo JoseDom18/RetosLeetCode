d06621790ba503ccae4548589c6a21783c5a1baf
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-07-12 12:59:45 -0600

    falta solucionar el caso en que si una es null continuar solo con la otra

--- 21.MergeTwoSortedLists/Solution21.java
+++ 21.MergeTwoSortedLists/Solution21.java
@@ -2,6 +2,7 @@ public class Solution21 {
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
     ListNode merge = null;
+
     if (list1 == null && list2 == null) {
       return merge;
     } else if (list1 == null) {
@@ -9,33 +10,60 @@ public class Solution21 {
     } else if (list2 == null) {
       return list1;
     } else {
-      if (list1.val < list2.val) {
-        merge = new ListNode(list1.val);
-      } else {
-        merge = new ListNode(list2.val);
-      }
-      ListNode tempMerge = merge;
-      ListNode tempList1 = list1;
-      ListNode tempList2 = list2;
 
-      while (tempList1 != null && tempList2 != null) {
-        if (tempList1.val < tempList2.val) {
-          tempMerge.next = new ListNode(tempList2.val);
+      // Inicio lista con el menor nodo comparando list1 y list2
+      merge = (list1.val < list2.val) ? new ListNode(list1.val) : new ListNode(list2.val);
+
+      // Avanzar la lista usada para iniciar
+      if (list1.val < list2.val) {
+        list1 = list1.next;
+      } else {
+        list2 = list2.next;
+      }
+
+      // Creo una lista para recorrer mi merge sin perder la cabeza
+      ListNode tempMerge = merge;
+
+      while (list1 != null || list2 != null) {
+
+        if (list1.val < list2.val) {
+          tempMerge.next = new ListNode(list1.val);
+          list1 = list1.next;
         } else {
-          tempMerge.next = new ListNode(tempList1.val);
+          tempMerge.next = new ListNode(list2.val);
+          list2 = list2.next;
         }
 
         tempMerge = tempMerge.next;
-        tempList1 = tempList1.next;
-        tempList2 = tempList2.next;
-        if (tempList1 != null) {
-          if (tempList1.val < tempList2.val) {
-            tempMerge.next = new ListNode(tempList1.val);
-          } else {
-            tempMerge.next = new ListNode(tempList2.val);
+
+        if (list1 == null) {
+          while (list2 != null) {
+            tempMerge.next = new ListNode(list2.val);
+            list2 = list2.next;
+          }
+        } else if (list2 == null) {
+          while (list1 != null) {
+            tempMerge.next = new ListNode(list1.val);
+            list1 = list1.next;
           }
         }
-        tempMerge = tempMerge.next;
+
+        // comprobación para evitar comparar int con null
+        // if (list1 != null && list2 != null) {
+        // } else if (list1 == null) {
+        // merge.next = new ListNode(list2.val);
+        // list2 = list2.next;
+        // if (list2 == null) {
+        // break;
+        // }
+        // } else {
+        // merge.next = new ListNode(list1.val);
+        // list1 = list1.next;
+        // if (list1 == null) {
+        // break;
+        // }
+        // }
+
       }
 
       return merge;
