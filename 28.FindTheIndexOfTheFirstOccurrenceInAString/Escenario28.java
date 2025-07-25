fa3fc5eb6c4080c89a8322f7f85d4b807f1ca97f
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-07-25 17:07:41 -0600

    completado

---
+++ 28.FindTheIndexOfTheFirstOccurrenceInAString/Escenario28.java
@@ -0,0 +1,9 @@
+public class Escenario28 {
+  
+  public static void main(String[] args) {
+    String haystack = "mississipi";
+    String needle = "issip";
+    Solution28.strStr(haystack, needle);
+  }
+
+}
--- 28.FindTheIndexOfTheFirstOccurrenceInAString/Solution28.java
+++ 28.FindTheIndexOfTheFirstOccurrenceInAString/Solution28.java
@@ -1,26 +1,20 @@
 public class Solution28 {
-  public int strStr(String haystack, String needle) {
-    int res = 0;
-    int j = 0;
-    char[] needChar = needle.toCharArray();
-    char[] hayChar = haystack.toCharArray();
+  public static int strStr(String haystack, String needle) {
 
-    if (haystack.length() < needle.length()) {
-      return -1;
-    }
+    String start1 = "";
+    int res = -1;
+    
     for (int i = 0; i < haystack.length(); i++) {
-      if (hayChar[i] == needChar[j]) {
-        if (j == 0) {
-          res = i;
-        }
-        if (j == needChar.length) {
-          return res;
-        }
-        j++;
-      } else {
-        j = 0;
+      if (i + needle.length() <= haystack.length()) {
+        start1 = haystack.substring(i, i + needle.length());
+      }
+
+      if (start1.equals(needle)) {
+        res = i;
+        return res;
       }
     }
-    return -1;
+
+    return res;
   }
 }
